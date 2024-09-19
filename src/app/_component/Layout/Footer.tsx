eaeb704097425dc255772f9bfb12140330165ae8
JUNU <bokjunwoo@gmail.com>
2024-09-20 00:23:30 +0900

    feat: GoogleAdSense 기능 추가

--- src/app/_component/Layout/Footer.tsx
+++ src/app/_component/Layout/Footer.tsx
@@ -3,44 +3,50 @@ import { grey } from '@mui/material/colors';
 import Image from 'next/image';
 import Link from 'next/link';
 import InquiryAvatarUI from '../Avatar/InquiryAvatarUI';
+import GoogleAdSense from '../Google/GoogleAd';
 
 const Footer = () => {
   return (
-    <Box
-      component="footer"
-      height="25vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      bgcolor={grey[300]}
-      color={grey[800]}
-      mt={3}
-    >
-      <Box px={2} display="flex" flexDirection="column" alignItems="center">
-        <InquiryAvatarUI />
-        <Typography variant="body2" color="inherit" mb={3}>
-          오류제보 | 개선사항 | 문의사항 | mapleburningfield@gmail.com
-        </Typography>
-        <Typography variant="body2" color="inherit" component="p" mb={0.5}>
-          &copy; 2024 버닝필드. All rights reserved.
-        </Typography>
-        <Box display="flex" alignItems="center" mb={0.5}>
-          <Typography variant="body2" color="inherit" component="p" mr={0.5}>
-            Data based on
-          </Typography>
-          <Link href="https://openapi.nexon.com/ko/" target="_blank">
-            <Image
-              src="/images/nexon_open_api.svg"
-              width={215}
-              height={13}
-              alt="nexon_open_api"
-            />
-          </Link>
-        </Box>
+    <Box width="100%" height="100%">
+      <Box my={3}>
+        <GoogleAdSense dataAdSlot="2469892538" />
+      </Box>
 
-        <Typography variant="body2" color="inherit" component="p">
-          This site is not associated with NEXON Korea.
-        </Typography>
+      <Box
+        component="footer"
+        height="25vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        bgcolor={grey[300]}
+        color={grey[800]}
+      >
+        <Box px={2} display="flex" flexDirection="column" alignItems="center">
+          <InquiryAvatarUI />
+          <Typography variant="body2" color="inherit" mb={3}>
+            오류제보 | 개선사항 | 문의사항 | mapleburningfield@gmail.com
+          </Typography>
+          <Typography variant="body2" color="inherit" component="p" mb={0.5}>
+            &copy; 2024 버닝필드. All rights reserved.
+          </Typography>
+          <Box display="flex" alignItems="center" mb={0.5}>
+            <Typography variant="body2" color="inherit" component="p" mr={0.5}>
+              Data based on
+            </Typography>
+            <Link href="https://openapi.nexon.com/ko/" target="_blank">
+              <Image
+                src="/images/nexon_open_api.svg"
+                width={215}
+                height={13}
+                alt="nexon_open_api"
+              />
+            </Link>
+          </Box>
+
+          <Typography variant="body2" color="inherit" component="p">
+            This site is not associated with NEXON Korea.
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
--- src/app/_component/Layout/Nav.tsx
+++ src/app/_component/Layout/Nav.tsx
@@ -4,6 +4,7 @@ import { Container, Box } from '@mui/material';
 import { usePathname } from 'next/navigation';
 import { useRecoilValue } from 'recoil';
 import NavLink from '../Box/Link/NavLink';
+import GoogleFixedAdSense from '../Google/GoogleFixedAd';
 import { characterInfoState } from '@/app/atoms/characterInfoState';
 
 const Nav = () => {
@@ -12,26 +13,34 @@ const Nav = () => {
   const characterInfo = useRecoilValue(characterInfoState);
 
   return (
-    <Box bgcolor="#27282E">
-      <Container maxWidth="xl">
-        <NavLink pathname={pathname} value="/" text="메인" />
+    <Box>
+      <Box bgcolor="#27282E">
+        <Container maxWidth="xl">
+          <NavLink pathname={pathname} value="/" text="메인" />
 
-        <NavLink
-          pathname={decodeURIComponent(pathname)}
-          value={`/character/${characterInfo.character_name}`}
-          text="사냥정보"
-        />
+          <NavLink
+            pathname={decodeURIComponent(pathname)}
+            value={`/character/${characterInfo.character_name}`}
+            text="사냥정보"
+          />
 
-        <NavLink
-          pathname={decodeURIComponent(pathname)}
-          value={`/exp/${characterInfo.character_name}`}
-          text="메할일"
-        />
+          <NavLink
+            pathname={decodeURIComponent(pathname)}
+            value={`/exp/${characterInfo.character_name}`}
+            text="메할일"
+          />
 
-        <NavLink pathname={pathname} value="/input" text="직접입력" />
+          <NavLink pathname={pathname} value="/input" text="직접입력" />
 
-        <NavLink pathname={pathname} value="/news" text="FAQ / 소식" />
-      </Container>
+          <NavLink pathname={pathname} value="/news" text="FAQ / 소식" />
+        </Container>
+      </Box>
+
+      {pathname !== '/' ? (
+        <Box width="100%" height={90} my={3}>
+          <GoogleFixedAdSense />
+        </Box>
+      ) : null}
     </Box>
   );
 };
