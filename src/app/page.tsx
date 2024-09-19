54bdef08bf7b5b0a1f457047d753eca280b4b64a
JUNU <bokjunwoo@gmail.com>
2024-09-20 00:23:44 +0900

    design: Home 컴포넌트에 Container 추가 및 레이아웃 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import MainSearchFormUI from './_component/Form/UI/MainSearchFormUI';
 
 const Home = () => {
   return (
-    <Box mb={-3}>
+    <Box>
       <Box
         width="100%"
         height="85vh"
@@ -27,32 +27,37 @@ const Home = () => {
           alignItems="center"
           justifyContent="center"
         >
-          <Typography
-            variant="h1"
-            color="white"
-            fontSize={48}
-            fontWeight="bold"
-            letterSpacing={10}
-            marginBottom={3}
-          >
-            버닝필드
-          </Typography>
-          <Typography variant="body1" color="white" marginBottom={3}>
-            메이플스토리 사냥터 정보 서비스
-          </Typography>
+          <Container maxWidth="xl">
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Typography
+                variant="h1"
+                color="white"
+                fontSize={48}
+                fontWeight="bold"
+                letterSpacing={10}
+                marginBottom={3}
+              >
+                버닝필드
+              </Typography>
+              <Typography variant="body1" color="white" marginBottom={3}>
+                메이플스토리 사냥터 정보 서비스
+              </Typography>
 
-          <Box mb={3}>
-            <MainSearchFormUI pathName="character" />
-          </Box>
+              <Box mb={3}>
+                <MainSearchFormUI pathName="character" />
+              </Box>
 
-          <Box>
-            <Typography variant="body2" fontSize={12} mb={1} color="white">
-              ❖ 200레벨 이상, 5차전직이 완료된 캐틱터만 이용가능합니다.
-            </Typography>
-            <Typography variant="body2" fontSize={12} mb={1} color="white">
-              ❖ 2023년 12월 21일 점검 이후 접속한 캐릭터만 조회가 가능합니다.
-            </Typography>
-          </Box>
+              <Box>
+                <Typography variant="body2" fontSize={12} mb={1} color="white">
+                  ❖ 200레벨 이상, 5차전직이 완료된 캐틱터만 이용가능합니다.
+                </Typography>
+                <Typography variant="body2" fontSize={12} mb={1} color="white">
+                  ❖ 2023년 12월 21일 점검 이후 접속한 캐릭터만 조회가
+                  가능합니다.
+                </Typography>
+              </Box>
+            </Box>
+          </Container>
         </Box>
       </Box>
     </Box>
