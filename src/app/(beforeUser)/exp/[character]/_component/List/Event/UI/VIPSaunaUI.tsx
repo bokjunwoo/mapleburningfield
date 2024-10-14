b5d30a7b6b21d73eb47a05031908f4145bbcf706
JUNU <bokjunwoo@gmail.com>
2024-10-14 23:58:58 +0900

    feat: VIPSauna/UI 컴포넌트 구현

---
+++ src/app/(beforeUser)/exp/[character]/_component/List/Event/UI/VIPSaunaUI.tsx
@@ -0,0 +1,82 @@
+import { SelectChangeEvent, Typography } from '@mui/material';
+import { grey, orange } from '@mui/material/colors';
+import { useRecoilValue } from 'recoil';
+import EventExpMapRadioUI from '../../../Radio/EventExpMapRadioUI';
+import ExpContentListUI from '../../common/ExpContentListUI';
+import EventMapListItemUI from './EventMapListItemUI';
+import { expectedExpRegionState } from '@/app/atoms/expectedExpState';
+import useRadio from '@/app/hooks/useRadio';
+import VIPSaunaTimeSettingItemUI from './VIPSaunaTimeSettingItemUI';
+import VIPSaunaListItemUI from './VIPSaunaListItemUI';
+import VIPSaunaRadioUI from '../../../Radio/VIPSaunaRadioUI';
+
+type Props = {
+  title: ExpContentTitle;
+  characterLevel: number;
+  selectValue: VIPSaunaUnit;
+  inputValue: number;
+  handleSelectChange: (event: SelectChangeEvent<VIPSaunaUnit>) => void;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const VIPSaunaUI = ({
+  title,
+  characterLevel,
+  selectValue,
+  inputValue,
+  handleSelectChange,
+  handleInputChange,
+}: Props) => {
+  const expectedExpRegion = useRecoilValue<ExpectedExpRegion<VIPSaunaRegion>[]>(
+    expectedExpRegionState(title),
+  );
+
+  const [expMultiplier, handleExpMultiplierChange] = useRadio(title);
+
+  return (
+    <ExpContentListUI title={title} characterLevel={characterLevel}>
+      <VIPSaunaRadioUI
+        value={expMultiplier}
+        handleChange={handleExpMultiplierChange}
+      />
+
+      <VIPSaunaTimeSettingItemUI
+        inputValue={inputValue}
+        handleInputChange={handleInputChange}
+        selectValue={selectValue}
+        handleSelectChange={handleSelectChange}
+      />
+
+      {expectedExpRegion.map((item) => (
+        <VIPSaunaListItemUI
+          key={item.region}
+          characterLevel={characterLevel}
+          item={item}
+        />
+      ))}
+
+      <Typography
+        variant="body2"
+        fontSize={12}
+        ml={0.5}
+        mt={0.5}
+        fontWeight={500}
+        color={grey[700]}
+      >
+        ❖ 실제 획득 경험치와 약간의 오차가 발생할 수 있습니다.
+      </Typography>
+      <Typography
+        variant="body2"
+        fontSize={12}
+        ml={0.5}
+        mt={0.5}
+        color={orange[900]}
+        fontWeight={500}
+      >
+        ❖ 이벤트 진행기간 24.10.17 ~ 24.12.18
+      </Typography>
+    </ExpContentListUI>
+  );
+};
+
+export default VIPSaunaUI;
---
+++ src/app/(beforeUser)/exp/[character]/_component/List/Event/VIPSauna.tsx
@@ -0,0 +1,69 @@
+import { SelectChangeEvent } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import EventExpMapUI from './UI/EventExpMapUI';
+import {
+  expectedExpMultiplier,
+  expectedExpRegionState,
+} from '@/app/atoms/expectedExpState';
+import { REGEX } from '@/app/constants/constants';
+import { EXP_CONTENT } from '@/app/constants/rate';
+import useValidatedInput from '@/app/hooks/useValidatedInput';
+import { calculateEventMapExp } from '@/app/utils/exp';
+import VIPSaunaUI from './UI/VIPSaunaUI';
+
+const VIPSauna = ({ characterLevel }: CharacterLevelProps) => {
+  const setExpectedExpRegion = useSetRecoilState<
+    ExpectedExpRegion<VIPSaunaRegion>[]
+  >(expectedExpRegionState(EXP_CONTENT.VIP_SAUNA));
+
+  const expMultiplier = useRecoilValue(
+    expectedExpMultiplier(EXP_CONTENT.VIP_SAUNA),
+  );
+
+  const [selectValue, setSelectValue] = useState<VIPSaunaUnit>('장');
+  const {
+    value: inputValue,
+    setValue,
+    handleChange: handleInputChange,
+  } = useValidatedInput(REGEX.NUMBER, selectValue === '분' ? 720 : 10);
+
+  const handleSelectChange = (event: SelectChangeEvent<VIPSaunaUnit>) => {
+    setSelectValue(event.target.value as VIPSaunaUnit);
+    setValue(0);
+  };
+
+  const eventMapExp = calculateEventMapExp(characterLevel);
+
+  const calculateCountValue = (value: number, unit: VIPSaunaUnit): number =>
+    unit === '장' ? value * 12 * 30 : value * 12;
+
+  const countValue = calculateCountValue(inputValue, selectValue);
+
+  const expectedExpRegionList: ExpectedExpRegion<VIPSaunaRegion>[] = [
+    {
+      region: '이용권 사용',
+      exp: eventMapExp.usedExp,
+      count: countValue,
+      checked: expMultiplier === '이용권 사용',
+      warning: false,
+    },
+  ];
+
+  useEffect(() => {
+    setExpectedExpRegion(expectedExpRegionList);
+  }, [expectedExpRegionList, setExpectedExpRegion]);
+
+  return (
+    <VIPSaunaUI
+      title={EXP_CONTENT.VIP_SAUNA}
+      characterLevel={characterLevel}
+      selectValue={selectValue}
+      inputValue={inputValue}
+      handleSelectChange={handleSelectChange}
+      handleInputChange={handleInputChange}
+    />
+  );
+};
+
+export default VIPSauna;
