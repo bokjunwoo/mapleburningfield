ce559ea186e0b439a2d16aef84e9e52742062c50
JUNU <bokjunwoo@gmail.com>
2024-10-14 23:58:40 +0900

    feat: VIPSaunaListItemUI 컴포넌트 구현

---
+++ src/app/(beforeUser)/exp/[character]/_component/List/Event/UI/VIPSaunaListItemUI.tsx
@@ -0,0 +1,32 @@
+import { Box } from '@mui/material';
+import ExpObtainableTextUI from '../../../Typography/ExpObtainableTextUI';
+import ExpItemIconUI from '../../common/ExpItemIconUI';
+import ExpListItemUI from '../../common/ExpListItemUI';
+import { calculateExpPercentage } from '@/app/utils/mob';
+
+type Props = {
+  item: ExpectedExpRegion<VIPSaunaRegion>;
+  characterLevel: number;
+};
+
+const VIPSaunaListItemUI = ({ item, characterLevel }: Props) => {
+  const expectedExpPercentage = calculateExpPercentage(
+    characterLevel,
+    item.exp * item.count,
+  );
+
+  return (
+    <ExpListItemUI isCheck={item.checked}>
+      <Box ml={1}>
+        <ExpItemIconUI region={item.region} folder="region_icon" />
+      </Box>
+
+      <ExpObtainableTextUI
+        region={item.region}
+        expPercentage={expectedExpPercentage}
+      />
+    </ExpListItemUI>
+  );
+};
+
+export default VIPSaunaListItemUI;
