faf262ffb9d14d4fd3ee994d6579a1f379db0bcf
JUNU <bokjunwoo@gmail.com>
2024-10-17 01:26:16 +0900

    feat: 경험치, 프리셋 AzmothPotion 컴포넌트 구현

---
+++ src/app/_component/List/Rate/Exp/AzmothPotion.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import RateSelectListItemUI from '../UI/RateSelectListItemUI';
+import { expRateState, expRateSelector } from '@/app/atoms/expRateState';
+import { RATE_NAME } from '@/app/constants/rate';
+import useRateSelect from '@/app/hooks/useRateSelcet';
+
+const AzmothPotion = () => {
+  const { value, handleRateChange } = useRateSelect({
+    rateSelector: expRateSelector,
+    state: expRateState,
+    rateName: RATE_NAME.AZMOTH_POTION,
+  });
+
+  const rateOption: SelectMenuItem = {
+    label: '아즈모스 영약',
+    key: RATE_NAME.AZMOTH_POTION,
+    items: [
+      { value: 0, label: '미적용' },
+      { value: 20, label: '적용 (+20%)' },
+    ],
+  };
+
+  return (
+    <RateSelectListItemUI
+      option={rateOption}
+      value={value}
+      handleChange={handleRateChange}
+    />
+  );
+};
+
+export default AzmothPotion;
---
+++ src/app/_component/List/Rate/Preset/AzmothPotion.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import RateSelectListItemUI from '../UI/RateSelectListItemUI';
+import {
+  presetExpRateState,
+  presetExpRateStateSelector,
+} from '@/app/atoms/presetRateState';
+import { RATE_NAME } from '@/app/constants/rate';
+import useRateSelect from '@/app/hooks/useRateSelcet';
+
+const AzmothPotion = () => {
+  const { value, handleRateChange } = useRateSelect({
+    rateSelector: presetExpRateStateSelector,
+    state: presetExpRateState,
+    rateName: RATE_NAME.AZMOTH_POTION,
+  });
+
+  const rateOption: SelectMenuItem = {
+    label: '아즈모스 영약',
+    key: RATE_NAME.AZMOTH_POTION,
+    items: [
+      { value: 0, label: '미적용' },
+      { value: 20, label: '적용 (+20%)' },
+    ],
+  };
+
+  return (
+    <RateSelectListItemUI
+      option={rateOption}
+      value={value}
+      handleChange={handleRateChange}
+    />
+  );
+};
+
+export default AzmothPotion;
