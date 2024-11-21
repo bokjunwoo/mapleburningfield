19f84c0e0857108c9d19d19df77fa082f01a4a16
JUNU <bokjunwoo@gmail.com>
2024-11-21 16:07:55 +0900

    feat: 새로운 사냥 이벤트 스킬로 변경(젠이츠와 이노스케)

--- src/app/constants/skill.ts
+++ src/app/constants/skill.ts
@@ -1,14 +1,14 @@
 export const EVENT_SKILL_INFO: SkillInfo = {
   ICON: 'event_skill',
-  NAME: '정예 기사',
-  DATE: '종료',
+  NAME: '젠이츠와 이노스케',
+  DATE: '2024.11.21 ~ 2024.12.18',
   MAIN_DESCRIPTION:
-    '레벨 범위 몬스터 300마리당 스킬 1회 발동하며 최대 15마리의 적을 1800% 데미지로 6번 공격하는 스킬이 10회 발동',
+    '레벨 범위 몬스터 200마리당 스킬 1회 발동하며 최대 15마리의 적을 1500% 데미지로 6번 공격하는 스킬 발동',
   TIME_DESCRIPTION: '캐릭터당 일일 30회',
-  EXP_DESCRIPTION: '스킬발동시 약 160마리 분량의 경험치 획득',
-  INITIAL_MOB_KILL: 160,
+  EXP_DESCRIPTION: '스킬발동시 약 180마리 분량의 경험치 획득',
+  INITIAL_MOB_KILL: 180,
   REQUIRED_MOB_COUNT: 300,
-  DELAY: 8,
+  DELAY: 5,
   ACTIVATION_COUNT: 30,
 };
 
