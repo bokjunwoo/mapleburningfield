abfe60996bdb5462632b8bec1ceb72677f5ae3e2
JUNU <bokjunwoo@gmail.com>
2024-12-19 02:30:53 +0900

    feat: 이벤트서버 Rate 컴포넌트 구현

---
+++ src/app/_component/List/Rate/Exp/EventWorld.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import RateSelectListItemUI from '../UI/RateSelectListItemUI';
+import { expRateState, expRateSelector } from '@/app/atoms/expRateState';
+import { RATE_NAME } from '@/app/constants/rate';
+import useRateSelect from '@/app/hooks/useRateSelcet';
+
+const EventWorld = () => {
+  const { value, handleRateChange } = useRateSelect({
+    rateSelector: expRateSelector,
+    state: expRateState,
+    rateName: RATE_NAME.EVENT_WORLD,
+  });
+
+  const rateOption: SelectMenuItem = {
+    label: '챌린저스 서버(경험치)',
+    key: RATE_NAME.EVENT_WORLD,
+    items: [
+      { value: 0, label: '미적용' },
+      { value: 50, label: '적용 (+50%)' },
+    ],
+  };
+
+  return (
+    <RateSelectListItemUI
+      option={rateOption}
+      value={value}
+      handleChange={handleRateChange}
+    />
+  );
+};
+
+export default EventWorld;
