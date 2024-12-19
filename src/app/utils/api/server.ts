92c74ee9cd150f9473d2f71837313067b950117d
JUNU <bokjunwoo@gmail.com>
2024-12-19 14:54:39 +0900

    feat: 챌린저스 서버 추가

--- src/app/utils/api/server.ts
+++ src/app/utils/api/server.ts
@@ -1,5 +1,8 @@
 export const findEventWorld = (data: CharacterInfoBase) => {
   const isEventWorld =
-    data.world_name === '챌린저스' || data.world_name === '챌린저스2';
+    data.world_name === '챌린저스' ||
+    data.world_name === '챌린저스2' ||
+    data.world_name === '챌린저스3' ||
+    data.world_name === '챌린저스4';
   return isEventWorld;
 };
