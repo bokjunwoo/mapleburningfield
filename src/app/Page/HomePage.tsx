841b7433af64a519c7bbb39e033a087b3453ee26
JUNU <bokjunwoo@gmail.com>
2025-02-20 09:32:19 +0800

    feat: 2025.02.20 업데이트

--- src/app/Page/HomePage.tsx
+++ src/app/Page/HomePage.tsx
@@ -59,7 +59,7 @@ const HomePage = () => {
                     color={yellow[400]}
                     textAlign="center"
                   >
-                    ❖ 01/16 신규 업데이트 안내
+                    ❖ 02/20 신규 업데이트 안내
                   </Typography>
                 </Link>
                 <Typography variant="body2" fontSize={12} mb={1} color="white">
